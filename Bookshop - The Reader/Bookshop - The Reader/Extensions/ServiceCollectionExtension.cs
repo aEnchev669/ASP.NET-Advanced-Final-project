71546d5dba35bb5b53f801fe7b50a676e432ddf9
aEnchev669 <aenchev669@abv.bg>
2024-03-10 12:44:00 +0300

    The Reader
    ServiceCollectionExtension added to simplify Program.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using Bookshop___The_Reader.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtension
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+
+			return services;
+		}
+		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+		{
+			var connectionString = config.GetConnectionString("DefaultConnection");
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(connectionString));
+
+			services.AddDatabaseDeveloperPageExceptionFilter();
+
+			return services;
+		}
+		public static IServiceCollection AddApplicationidentity(this IServiceCollection services, IConfiguration config)
+		{
+
+			services
+				.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			return services;
+		}
+	}
+}
--- Bookshop - The Reader/Bookshop - The Reader/Program.cs
+++ Bookshop - The Reader/Bookshop - The Reader/Program.cs
@@ -5,13 +5,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
