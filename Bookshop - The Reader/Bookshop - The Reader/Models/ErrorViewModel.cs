0f350d8df952437cab4e49d9a4115e7fe0669305
aEnchev669 <aenchev669@abv.bg>
2024-03-10 12:22:26 +0300

    Create ErrorViewModel.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookshop___The_Reader.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
