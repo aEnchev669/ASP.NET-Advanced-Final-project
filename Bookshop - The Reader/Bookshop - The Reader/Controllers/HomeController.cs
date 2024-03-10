db7d8a2acc7704c7b8935ee497a57976c8b9be29
aEnchev669 <aenchev669@abv.bg>
2024-03-10 12:22:19 +0300

    Create HomeController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Bookshop___The_Reader.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bookshop___The_Reader.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
