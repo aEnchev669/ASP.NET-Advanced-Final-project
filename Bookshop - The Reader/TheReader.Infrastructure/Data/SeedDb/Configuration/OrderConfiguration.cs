e1fe2f1379403d626203582197f2fb86b333c2a6
aEnchev669 <aenchev669@abv.bg>
2024-03-13 22:01:51 +0300

    Create OrderConfiguration.cs

---
+++ Bookshop - The Reader/TheReader.Infrastructure/Data/SeedDb/Configuration/OrderConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TheReader.Infrastructure.Data.Models.Orders;
+
+namespace TheReader.Infrastructure.Data.SeedDb.Configuration
+{
+	internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+	{
+		public void Configure(EntityTypeBuilder<Order> builder)
+		{
+			builder
+				.Property(o => o.TotalPrice)
+				.HasPrecision(18, 2);
+		}
+	}
+}
