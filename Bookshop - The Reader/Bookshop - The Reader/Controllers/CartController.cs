01408e419560f5b9d9fc7593a27514d8e7b525ad
aEnchev669 <aenchev669@abv.bg>
2024-03-18 22:35:36 +0300

    Create CartController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TheReader.Infrastructure.Extensions;
+
+namespace Bookshop___The_Reader.Controllers
+{
+	[Authorize]
+	public class CartController : Controller
+	{
+		[HttpGet]
+		public async Task<IActionResult> Cart()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Remove(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Increase(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Decrease(int id)
+		{
+			return View();
+		}
+		private IActionResult GeneralErrorMessage()
+		{ 
+			return View();
+		}
+	}
+}
