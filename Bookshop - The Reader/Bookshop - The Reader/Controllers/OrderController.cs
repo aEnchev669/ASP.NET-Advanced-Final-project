9da030d38f09c84c0817c1a35d2156c951051573
aEnchev669 <aenchev669@abv.bg>
2024-03-18 22:35:34 +0300

    Create OrderController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheReader.Core.Models.Order;
+
+namespace Bookshop___The_Reader.Controllers
+{
+	public class OrderController : Controller
+	{
+		[HttpGet]
+		public async Task<IActionResult> Checkout()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Checkout(OrderFormViewModel order)
+		{
+			return View(order);
+		}
+
+		[HttpGet]
+		public IActionResult Confirmed()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> OrderHistory()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> OrderDetails(string id)
+		{
+			return View();
+		}
+	}
+}
