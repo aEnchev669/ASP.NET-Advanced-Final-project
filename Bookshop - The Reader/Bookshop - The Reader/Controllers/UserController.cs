038c20d6d62d747c4932cc1b07ee1dd26ec9ae2f
aEnchev669 <aenchev669@abv.bg>
2024-03-18 22:35:31 +0300

    Create UserController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshop___The_Reader.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
