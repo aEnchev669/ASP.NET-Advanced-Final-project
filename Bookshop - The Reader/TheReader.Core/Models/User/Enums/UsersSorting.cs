39b9be97c00510d217270e70b0a79c0b20d394bf
aEnchev669 <aenchev669@abv.bg>
2024-03-18 22:36:07 +0300

    Create UsersSorting.cs

---
+++ Bookshop - The Reader/TheReader.Core/Models/User/Enums/UsersSorting.cs
@@ -0,0 +1,11 @@
+﻿namespace TheReader.Core.Models.User.Enums
+{
+    public enum UsersSorting
+    {
+        CreatedDescending = 0,
+        CreatedAscending = 1,
+        NameAscending = 2,
+        NameDescending = 3
+
+    }
+}
