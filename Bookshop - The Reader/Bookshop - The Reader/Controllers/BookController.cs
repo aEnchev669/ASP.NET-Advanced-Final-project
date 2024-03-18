06a8fa22ad36e9af079102b21d6c41c9c10d84a4
aEnchev669 <aenchev669@abv.bg>
2024-03-18 22:35:37 +0300

    Create BookController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TheReader.Core.Models.Book;
+
+namespace Bookshop___The_Reader.Controllers
+{
+	[Authorize]
+	public class BookController : Controller
+	{
+		[AllowAnonymous]
+		public async Task<IActionResult> All()
+		{
+			var model = new AllBooksViewModel();
+			return View(model);
+		}
+		public async Task<IActionResult> Favourites()
+		{
+			var model = new AllBooksViewModel();
+			return View(model);
+		}
+
+		public async Task<IActionResult> Details(int id)
+		{
+			var model = new BooksDetailsViewModel();
+			return View(model);
+		}
+		public async Task<IActionResult> Reviews(int id)
+		{
+			var model = new BooksDetailsViewModel();
+			return View(model);
+		}
+		private IActionResult GeneralErrorMessage()
+		{
+			
+			return View();
+		}
+	}
+}
