790552dc673d001804036c2f83db5fc10cd418be
aEnchev669 <aenchev669@abv.bg>
2024-03-18 22:35:58 +0300

    Create ApplicaitonConstants.cs

---
+++ Bookshop - The Reader/TheReader.Infrastructure/Constants/ApplicaitonConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace TheReader.Infrastructure.Constants
+{
+    public static class ApplicaitonConstants
+    {
+        public const int ReleaseYear = 2024;
+
+        public const int DefaultPage = 1;
+        public const int ItemsPerPageConstant = 12;
+
+        public const int PasswordMinLength = 6;
+        public const int MaxFailedAccessAttemptsConst = 3;
+
+        public const string AdministratorUsername = "Admin";
+        public const string AdministratorEmail = "administrator@abv.bg";
+        public const string AdministratorPassword = "Admin1234";
+        public const string AdministratorRole = "Admin";
+    }
+}
