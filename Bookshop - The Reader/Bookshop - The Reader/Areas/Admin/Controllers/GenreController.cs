17ff7e4c954859fe0b2ee2c99977e4884ad62ec6
aEnchev669 <aenchev669@abv.bg>
2024-03-20 23:37:10 +0300

    Create GenreController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheReader.Core.Models.Genre;
+
+namespace Bookshop___The_Reader.Areas.Admin.Controllers
+{
+	public class GenreController : BaseController
+	{
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(NewGenreViewModel genreModel)
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, NewGenreViewModel genreModel)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(int id)
+		{
+			return View();
+		}
+
+		private IActionResult GeneralErrorMessage()
+		{
+			return View();
+		}
+	}
+}
