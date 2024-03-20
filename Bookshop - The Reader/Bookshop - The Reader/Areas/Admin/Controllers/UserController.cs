e0441670039cd7103acab4983b5dc3f520086530
aEnchev669 <aenchev669@abv.bg>
2024-03-20 23:37:11 +0300

    Create UserController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshop___The_Reader.Areas.Admin.Controllers
+{
+	public class UserController : BaseController
+	{
+		[HttpPost]
+		public async Task<IActionResult> Delete(string id)
+		{
+			return View();
+		}
+	}
+}
