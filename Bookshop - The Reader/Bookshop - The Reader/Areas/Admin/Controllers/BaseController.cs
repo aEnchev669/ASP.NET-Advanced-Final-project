2b577a4ac76e71d2fcd02e580e0cb396d8b78362
aEnchev669 <aenchev669@abv.bg>
2024-03-20 23:37:07 +0300

    Create BaseController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshop___The_Reader.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Admin, Moderator")]
+	public class BaseController : Controller
+	{
+	}
+}
