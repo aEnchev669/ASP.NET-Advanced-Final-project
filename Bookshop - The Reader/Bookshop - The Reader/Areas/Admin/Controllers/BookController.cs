4b8f99bd313fbe0d43108da13f807d45f9453f24
aEnchev669 <aenchev669@abv.bg>
2024-03-20 23:37:08 +0300

    Create BookController.cs

---
+++ Bookshop - The Reader/Bookshop - The Reader/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheReader.Core.Models.Book;
+
+namespace Bookshop___The_Reader.Areas.Admin.Controllers
+{
+	public class BookController : BaseController
+	{
+		[HttpGet]
+		public async Task<IActionResult> Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(BookFormViewModel itemModel)
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, BookFormViewModel itemModel)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(int id)
+		{
+			return View();
+		}
+
+		private IActionResult GeneralErrorMessage()
+		{
+			return View();
+		}
+	}
+}
