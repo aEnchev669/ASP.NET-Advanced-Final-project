e83479365fd0a5112c8f70368c54085b90636032
aEnchev669 <aenchev669@abv.bg>
2024-04-05 20:12:15 +0300

    Update ServiceCollectionExtension.cs

--- Bookshop - The Reader/Bookshop - The Reader/Extensions/ServiceCollectionExtension.cs
+++ Bookshop - The Reader/Bookshop - The Reader/Extensions/ServiceCollectionExtension.cs
@@ -2,6 +2,10 @@
 using BookshopTheReader.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TheReader.Core.Contracts.Book;
+using TheReader.Core.Models.Book.Enums;
+using TheReader.Core.Services;
+using TheReader.Infrastructure.Data.Common;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -9,6 +13,8 @@ namespace Microsoft.Extensions.DependencyInjection
 	{
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
+			services.AddScoped<IBookService, BookService>();
+
 			return services;
 		}
 		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
@@ -16,6 +22,8 @@ namespace Microsoft.Extensions.DependencyInjection
 			var connectionString = config.GetConnectionString("DefaultConnection");
 			services.AddDbContext<TheReaderDbContext>(options =>
 				options.UseSqlServer(connectionString));
+
+			services.AddScoped<IRepository, Repository>();
 
 			services.AddDatabaseDeveloperPageExceptionFilter();
 
